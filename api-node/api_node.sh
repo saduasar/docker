bc0c2d9ee27f988155aab4a76ce17bd78f20fb51
saduasar <126820557+saduasar@users.noreply.github.com>
2024-02-11 11:21:05 -0500

    Update api_node.sh

--- api-node/api_node.sh
+++ api-node/api_node.sh
@@ -4,7 +4,7 @@
 container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgresDB)
 
 # Creating DATABASE_URL with the retrieved container IP
-DATABASE_URL="postgres://postgres:mysecretpassword@$container_ip:5432/postgres"
+DATABASE_URL="postgres://postgres:mysecretpassword@${container_ip}:5432/postgres"
 
 
 #!/bin/bash
