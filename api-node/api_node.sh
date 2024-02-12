e170529414b7d9b0cd57a2136b676f7b5d0df497
saduasar <126820557+saduasar@users.noreply.github.com>
2024-02-11 23:27:16 -0500

    Update api_node.sh

--- api-node/api_node.sh
+++ api-node/api_node.sh
@@ -3,6 +3,13 @@
 
 # Creating DATABASE_URL with the retrieved container IP
 DATABASE_URL="postgres://postgres:mysecretpassword@172.17.0.2:5432/postgres"
+#'DATABASE_URL: This is an environment variable used to store the connection URL or string for accessing a database.
+#"postgres://postgres:mysecretpassword@172.17.0.2:5432/postgres": This is the actual connection URL, which consists of several components:
+#postgres://: This is the protocol or scheme used to access the database. In this case, it's PostgreSQL's default protocol.
+#postgres: This is the username used to authenticate with the database server.
+#mysecretpassword: This is the password used to authenticate with the database server.
+#@172.17.0.2:5432: This is the host and port of the database server. In this case, it's 172.17.0.2 (an IP address) and port 5432.
+#/postgres: This is the name of the database to connect to.
 
 #!/bin/bash
 
