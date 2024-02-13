7d5c4ab6c25de7912b9a85e11c0291e360b93ef1
saduasar <126820557+saduasar@users.noreply.github.com>
2024-02-13 14:25:03 -0500

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -12,10 +12,10 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
 #Install docker's latest version
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 #Verify the success of docker installation
 sudo docker run hello-world
